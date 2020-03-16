68518b27bbe1632e9bf82b024d35db78f12b4d16
Hamza Jabbar <hamza.jabbar@square63.com>
2020-03-16 17:41:27 +0500

    Updates

---
+++ precache-manifest.86f1da1ecb5c6edb4fa796573cd09a85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc6ae9e516b43ece9162e4706c41921",
+    "url": "/brandsintown/index.html"
+  },
+  {
+    "revision": "dac1035bdab0a8445392",
+    "url": "/brandsintown/static/css/main.1b2a865f.chunk.css"
+  },
+  {
+    "revision": "4c8899805ee58746c6a8",
+    "url": "/brandsintown/static/js/2.7cc296eb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/brandsintown/static/js/2.7cc296eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac1035bdab0a8445392",
+    "url": "/brandsintown/static/js/main.2faa4c21.chunk.js"
+  },
+  {
+    "revision": "feddbc08927cf0c5d211",
+    "url": "/brandsintown/static/js/runtime-main.dbc57c59.js"
+  },
+  {
+    "revision": "601f57dc28ae14a1400b9f4c1f45dfab",
+    "url": "/brandsintown/static/media/search.601f57dc.svg"
+  }
+]);
